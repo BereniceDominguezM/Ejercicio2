2132a946968ca2897343716e0ce7ee65475f3e13
BereniceDominguezM <167391428+BereniceDominguezM@users.noreply.github.com>
2024-05-13 22:41:14 -0600

    Add files via upload

---
+++ Tarea2.php/encabezado.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="UTF-8">
+ <title>Agenda de Deportes</title>
+ <style>
+ body {
+    font-family: Arial, sans-serif;
+    background-color: cornsilk;
+    margin: 0;
+    padding: 0;
+ }
+ 
+ header {
+    background-color: floralwhite;
+    color: #333;
+    padding: 1px 0;
+    text-align: center;
+ }
+ 
+ #titulo {
+    font-size: 30px;
+    color: #333;
+    margin-top: 10px;
+    text-align: center;
+ }
+ 
+ img {
+    display: block;
+    margin: 10px auto;
+    border: 2px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+ }
+ </style>
+ </head>
+ <body>
+ <header>
+    <h1>AGENDA DE DEPORTES</h1>
+ </header>
+ 
+ <img src="deportes.jpg" width="800" height="200" />
+ 
+ </body>
+</html>
---
+++ Tarea2.php/pie.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+        }
+        #centrado {
+            margin: 20px 0; /* Espacio superior e inferior */
+        }
+        #centrado a {
+            margin: 0 15px; /* Espacio a la izquierda y derecha de cada enlace */
+            text-decoration: none; /* Elimina el subrayado de los enlaces */
+            color: #000; /* Color del texto de los enlaces */
+        }
+        #centrado a:hover {
+            text-decoration: underline; /* Subrayado al pasar el ratón por encima */
+        }
+        h5 {
+            margin-top: 40px; /* Espacio superior para el texto del pie de página */
+        }
+    </style>
+</head>
+<body>
+    <p id="centrado">
+        <a href="principal.php">Principal</a> | 
+        <a href="#">Registro del Deporte</a> | 
+        <a href="#">Listado de Deportes</a> | 
+        <a href="javascript:close()">Salir</a> 
+    </p>
+    <h5>Todos los derechos reservados @2025 Diseñado por Berenice Dominguez</h5>
+</body>
+</html>
